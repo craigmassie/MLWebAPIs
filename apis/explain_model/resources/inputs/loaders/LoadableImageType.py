0cf2e6d518a770663b50f0e23b2a67cc7722f37b
craig <craigsmassie@gmail.com>
2022-01-24 12:23:03 +0000

    updated usage to resnet

--- apis/explain_model/resources/inputs/loaders/LoadableImageType.py
+++ apis/explain_model/resources/inputs/loaders/LoadableImageType.py
@@ -2,8 +2,8 @@ from typing import Union
 from typing_extensions import Annotated
 from apis.explain_model.resources.inputs.loaders.ImagenetLoadableImage import ImagenetLoadableImage
 from pydantic import BaseModel, Field
-from apis.explain_model.resources.inputs.loaders.ResnetLoadableImage import TensorflowLoadableImage
+from apis.explain_model.resources.inputs.loaders.ResnetLoadableImage import ResnetLoadableImage
 
 
 class LoadableImageType(BaseModel):
-    image: Annotated[Union[ImagenetLoadableImage, TensorflowLoadableImage], Field(discriminator='type')]
+    image: Annotated[Union[ImagenetLoadableImage, ResnetLoadableImage], Field(discriminator='type')]
